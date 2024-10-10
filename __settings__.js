4c81356ce070708034dd62b1255f9d768168e60c
Sebastian Heath <sebastian.heath@gmail.com>
2024-10-09 22:44:01 -0400

    1st

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2084237.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 198313666, 198313653 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/198313703/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/198313700/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/198313702/1/ammo.js', 'preload' : true},
+];
